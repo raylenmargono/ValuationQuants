772d28f17c4a6d0a948154f468eb923af44ef357
Raylen Margono <raylenmargono@gmail.com>
2017-06-17 15:31:29 -0400

    static root

--- ValuationQuants/settings.py
+++ ValuationQuants/settings.py
@@ -137,6 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 CORS_ORIGIN_WHITELIST = (
     '127.0.0.1:8000'
