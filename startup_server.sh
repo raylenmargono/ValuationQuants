5c5de014e74bc055b86e41dbe97f16e6ccf48ac2
Raylen Margono <raylenmargono@gmail.com>
2017-08-15 23:32:03 -0400

    collect static

--- startup_server.sh
+++ startup_server.sh
@@ -4,5 +4,6 @@ source env/bin/activate
 cp ./nginx/default /etc/nginx/sites-enabled
 git pull
 python manage.py migrate
+python manage.py collectstatic --noinput
 service nginx restart
 service gunicorn restart
